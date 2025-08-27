40be468cf1a5c78f7d46b3feef3e8ca47783894a
haridinesh <haridinesh08@gmail.com>
2025-08-27 18:22:53 -0400

    fix the warnings from banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
-import { ArrowRightCircle } from "react-bootstrap-icons";
 import developerImg from "../assets/img/developer.svg";
 import resume from "../content/Resume.pdf";
 
@@ -11,7 +10,6 @@ export const Banner = () => {
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio!</span>
                         <h1>I'm Hari</h1>
-                        <h2></h2>
                         <p>A Full Stack Developer with 8+ years of experience building scalable applications using .NET Core, C#, React, SQL Server, and Azure. I specialize in developing end-to-end solutions—from designing APIs and optimizing databases to creating responsive UIs and deploying cloud-native applications. As a <a href="https://learn.microsoft.com/en-ca/users/HariDineshKrishnamurthy-3635/credentials/259BD18E566E5FF6">Microsoft Certified: Azure AI Engineer Associate</a>, I’m passionate about integrating AI-powered automation into modern software to improve efficiency and user experience.</p>
                         {/* <button onClick={() => window.location="#connect"}>Let's connect <ArrowRightCircle size={25}/></button> */}
                         <a className="resume-btn" href={resume} download><Button variant="dark">Resume</Button></a>
